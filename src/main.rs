e9cfc4718b3cd9bec7dd3c8783329d793d64baaf
AmilsonJunior <junior.amilson@gmail.com>
2023-11-15 04:40:58 -0300

    echo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
+use std::fmt::format;
 use std::io::{Write, BufReader, BufRead};
 use std::net::{TcpListener, TcpStream};
+
+fn build_response(status: &str, content_type: Option<&str>, content: Option<&str>) -> String {
+    let mut response_lines = Vec::<String>::new();
+    response_lines.push(format!("HTTP/1.1 {:}", status));
+
+    if content_type.is_some() {
+        response_lines.push(format!("Content-Type: {}", content_type.unwrap()));
+    }
+
+    if content.is_some() {
+        response_lines.push(format!("Content-Length: {}\r\n", content.unwrap().bytes().len()));
+        response_lines.push(content.unwrap().to_string());
+    }
+
+    response_lines.push(String::from("\r\n"));
+    let response = response_lines.join("\r\n");
+
+    response
+}
 
 fn handle_connection(mut stream: TcpStream) {
     println!("accepted new connection");
@@ -14,12 +34,19 @@ fn handle_connection(mut stream: TcpStream) {
     let (method, rest) = http_request.first().unwrap().split_once(" ").expect("Unable to get Method from Http request");
     let (path, _) = rest.split_once(" ").expect("Unable to get Path from Http request");
 
-    let mut response = "HTTP/1.1 404 Not Found\r\n\r\n";
-    if path == "/" {
-        response = "HTTP/1.1 200 OK\r\n\r\n";
-    }
+    match path {
+        "/" => {
+            stream.write_all(build_response("200 OK", None, None).as_bytes()).unwrap();
+        },
+        path if path.contains("/echo/") => {
+            let (_, echo_value) = path.split_once("/echo/").unwrap();
 
-    stream.write_all(response.as_bytes()).unwrap();
+            stream.write_all(build_response("200 OK", Some("text/plain"), Some(echo_value)).as_bytes()).unwrap();
+        }
+        _ => {
+            stream.write_all(build_response("404 Not Found", None, None).as_bytes()).unwrap();
+        }
+    }
 }
 
 fn main() {
